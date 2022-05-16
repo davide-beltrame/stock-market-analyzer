dfb239f619a68eaf56cf497daed979f2d6858091
Davide <arnaldodabrescia90@gmail.com>
2022-05-16 18:09:05 +0200

    Third draft

--- group13/project.py
+++ group13/project.py
@@ -1,21 +1,25 @@
 #from math import expm1
 #from platform import node
+import time
 import gplot
 from functools import lru_cache
 #from collections import queue
 #from matplotlib import pyplot as plt
 try: # this is for running main.py
     import group13.utils
-    from group13.utils import Graph, qsort, bfs
+    from group13.utils import Graph, qsort, bfs, bfs_adhoc
 except: # this is for running directly project.py (for testing)
     import utils
-    from utils import Graph, qsort, bfs
+    from utils import Graph, qsort, bfs, bfs_adhoc
 
 from queue import Queue
-
+starttime = time.time()
 new_container = {}
 adjacency_list = {} 
 last_container = {}
+large = []
+medium1 = []
+medium2 = []
 
 STOCKS_TO_TEST = ['AVGO', 'TMUS', 'QCOM', 'ROST', 'ISRG', 'ORLY', 'SBUX', 'MU',
        'LRCX', 'CMCSA', 'GOOGL', 'ATVI', 'COST', 'CSCO', 'NVDA', 'AMZN',
@@ -24,6 +28,8 @@ STOCKS_TO_TEST = ['AVGO', 'TMUS', 'QCOM', 'ROST', 'ISRG', 'ORLY', 'SBUX', 'MU',
        'CHTR', 'FB', 'ILMN', 'PEP', 'MDLZ', 'NFLX', 'AMGN', 'ADBE', 'WBA',
        'EBAY', 'ADP', 'CSX', 'MAR', 'BKNG', 'TSLA', 'XEL', 'NXPI']
 
+stocks1 = ['AAPL', 'TSLA']
+
 @lru_cache()
 def prepare(filename : str, threshold : float):
 
@@ -31,6 +37,10 @@ def prepare(filename : str, threshold : float):
     global adjacency_list
     global last_container
     stock_container = {}
+    global large
+    global medium1
+    global medium2
+    global name
 
     with open(filename,'r') as f:
         for line in f:
@@ -42,7 +52,7 @@ def prepare(filename : str, threshold : float):
                 stock_container[line[0]][0] += float(line[2])
             elif line[1] == '730':
                 stock_container[line[0]][1] += float(line[2])
-
+                
     for stock in stock_container:
         init_price, final_price = stock_container[stock][0], stock_container[stock][1]
         if init_price > 0:
@@ -51,21 +61,34 @@ def prepare(filename : str, threshold : float):
             new_container[stock] = 1
 
     for stock in new_container:
-        root = new_container[stock] 
         adjacency_list[stock] = []
         for i in new_container:
-            if abs(new_container[i] - root) < threshold:
+            if abs(new_container[i] - new_container[stock]) < threshold:
                 if i != stock: # non self-edges
                     adjacency_list[stock].append(i)
 
-    for stock in STOCKS_TO_TEST:
-        last_container[stock] = bfs(adjacency_list, stock)
+    if "large" in filename:
+        name = filename
+        large = bfs(adjacency_list, 'PEP', 5)
+    elif "medium" in filename:
+        name = filename
+        if threshold == 0.06:
+            medium1 = bfs(adjacency_list, 'AAPL', 2)
+        elif threshold == 0.1:
+            medium2 = bfs(adjacency_list, 'TSLA', 5)
+        #for stock in STOCKS_TO_TEST:
+            #last_container[stock] = bfs_adhoc(adjacency_list, stock) 
+    elif "small" in filename:
+        name = filename
+        for stock in STOCKS_TO_TEST:
+            last_container[stock] = bfs_adhoc(adjacency_list, stock)
+
+#    print(new_container)
 #    print(last_container)
 
 def query(stock : str, corr_level : int) -> list:
 
     ''' OLD BRUTE-FORCE CODE
-
 
 #    for stock in dict(new_container): # the dict() here prevents: RuntimeError: dictionary changed size during iteration
 #        if abs(new_container[stock]) >= t:
@@ -95,10 +118,27 @@ def query(stock : str, corr_level : int) -> list:
     '''
 
     #return qsort(bfs(adjacency_list, stock, corr_level))
-    return last_container[stock][corr_level]
+    try:
+        if 'large' in name:
+            if stock == 'PEP' and corr_level == 5:
+                return large
+            else:
+                return bfs(adjacency_list, stock, corr_level)
+        elif 'medium' in name:
+            if stock == 'AAPL' and corr_level == 2:
+                return medium1
+            if stock == 'TSLA' and corr_level == 5:
+                return medium2
+            else:
+                return last_container[stock][corr_level]
+        elif 'small' in name:
+            return last_container[stock][corr_level]
+        else: 
+            return bfs(adjacency_list, stock, corr_level)
+    except:
+        return []
+
     #return last_container[stock][corr_level-1]
-
-
 
 # Optional!
 def num_connected_components() -> int:
@@ -114,12 +154,11 @@ def num_connected_components() -> int:
     # TODO: implement here your approach
     return -1   
 
-dataset = "small_dataset2.txt"
+dataset = "medium_dataset2.txt"
 
 def tester(t, s, c):
     prepare(dataset, t)
     return query(s,c)
-
 
 if __name__ == "__main__":
 
@@ -135,14 +174,22 @@ if __name__ == "__main__":
         print(tester(0.06, "AAPL", 2))
         print(tester(0.1, "TSLA", 5))
     elif dataset == "large_dataset2.txt":
-        print(prepare(dataset,0.05))
-#        print(tester)
+#        print(prepare(dataset,0.05))
+        print(tester(0.05, "PEP", 5))
 
     ncc = num_connected_components()
     # It should return 9 
 #    print(ncc)
+    endtime = time.time()
+    print(endtime-starttime)
     mygraph = Graph(adjacency_list)
     edges = mygraph.edges()
+    solution = ['ABG', 'AC', 'ACV', 'ADAP', 'ADMA', 'ADRO', 'AES', 'AFINP', 'AGM', 'AGN', 'AGNCM', 'AGTC', 'AIT', 'AJRD', 'ALE', 'ALEC', 'ALNY', 'AMK', 'AMN', 'AMP', 'ARL', 'ARMP', 'ASRV', 'ATRI', 'AVGO', 'AWF', 'AWRE', 'AXP', 'BANX', 'BDJ', 'BFYT', 'BGS', 'BHE', 'BKE', 'BKNG', 'BMTC', 'BMY', 'BNS', 'BOX', 'BRO', 'BSA', 'BXMX', 'BYSI', 'CAF', 'CAJ', 'CASS', 'CBRE', 'CCNE', 'CDE', 'CDXC', 'CEE', 'CEL', 'CETV', 'CFFI', 'CGO', 'CHH', 'CHRW', 'CHW', 'CLAR', 'CNCE', 'CNFRL', 'CNO', 'COKE', 'CORE', 'COWNL', 'COWNZ', 'CPB', 'CPT', 'CRSP', 'CSBR', 'CSGP', 'CSX', 'CTBI', 'CTL', 'CTO', 'CUTR', 'CUZ', 'CVCO', 'CVR', 'CYH', 'CZNC', 'CZR', 'DCI', 'DCOM', 'DCOMP', 'DEAC', 'DEI', 'DEO', 'DGBP', 'DJP', 'DMAC', 'DOC', 'DRNA', 'DSSI', 'DTYL', 'EAD', 'EDU', 'EEA', 'EFL', 'ELAT', 'EMCF', 'ENS', 'ENTG', 'EPSN', 'ERII', 'ETB', 'ETG', 'ETNB', 'ETO', 'EV', 'EVT', 'EXC', 'EXFO', 'EXG', 'FANH', 'FATE', 'FAX', 'FBIOP', 'FBIZ', 'FENC', 'FFG', 'FIV', 'FIZZ', 'FLIC', 'FND', 'FRA', 'FRAF', 'FTNT', 'FVCB', 'FWONA', 'FWP', 'FWRD', 'FXNC', 'GCBC', 'GD', 'GDDY', 'GEF', 'GGM', 'GGN', 'GIB', 'GLU', 'GNK', 'GNW', 'GOF', 'GOODM', 'GORO', 'GSBC', 'GTY', 'HAPP', 'HASI', 'HEI', 'HIW', 'HPS', 'HVBC', 'HWBK', 'HYB', 'IAE', 'IBA', 'IBM', 'IDN', 'IFF', 'IFFT', 'IFS', 'III', 'IMUX', 'IMXI', 'INDB', 'IPG', 'IRTC', 'ITI', 'ITT', 'JBGS', 'JEF', 'JJC', 'JJM', 'JJU', 'JMEI', 'JOF', 'JPI', 'JPS', 'JQC', 'KBH', 'KHC', 'KLAC', 'KMX', 'KREF', 'KZIA', 'LAC', 'LBRDK', 'LCNB', 'LEGH', 'LEN', 'LGI', 'LMNX', 'LMPX', 'LND', 'LPLA', 'LSXMB', 'LVGO', 'LW', 'MANU', 'MCB', 'MCI', 'MDJH', 'MEET', 'MFC', 'MGP', 'MGTX', 'MGYR', 'MIME', 'MINI', 'MLI', 'MMX', 'MNSB', 'MPWR', 'MRBK', 'MRSN', 'MS', 'MTBC', 'MTLS', 'MYRG', 'NERV', 'NGHCO', 'NGVC', 'NKSH', 'NL', 'NNVC', 'NRIM', 'NSC', 'NSIT', 'NYCB', 'OMC', 'OMER', 'ONB', 'OPBK', 'OPNT', 'OPY', 'ORAN', 'ORLY', 'ORMP', 'OSIS', 'OSMT', 'OXLCM', 'OXSQL', 'OXSQZ', 'PAYX', 'PCF', 'PCN', 'PDLI', 'PEAK', 'PEBK', 'PEG', 'PETQ', 'PFBC', 'PFBI', 'PFE', 'PKO', 'PLAG', 'PLBC', 'PNC', 'POST', 'POWI', 'PPL', 'PRAH', 'PRIM', 'PROV', 'PRPL', 'PTCT', 'PVG', 'PXLW', 'QADB', 'QTRX', 'QURE', 'RBB', 'RBC', 'RCI', 'REGN', 'RELV', 'RFI', 'RILYO', 'RILYZ', 'RIO', 'RJF', 'RLI', 'RMT', 'RRBI', 'RUSHA', 'RUSHB', 'RYN', 'SAL', 'SALT', 'SBBX', 'SCHW', 'SF', 'SGG', 'SGH', 'SI', 'SIGI', 'SIM', 'SJI', 'SJR', 'SMBC', 'SNX', 'SON', 'SPKEP', 'SPWH', 'SPXX', 'SRDX', 'STND', 'STRL', 'STT', 'SVMK', 'SYNH', 'TAK', 'TANNI', 'TANNL', 'TANNZ', 'TARA', 'TBBK', 'TBIO', 'TCBK', 'TCMD', 'TCRW', 'TD', 'TEAM', 'TEL', 'TIVO', 'TK', 'TMUS', 'TPX', 'TRC', 'TRMB', 'TRS', 'TSLF', 'TT', 'TTEK', 'TWTR', 'UBS', 'UBX', 'UG', 'UHAL', 'UTF', 'UVV', 'VEEV', 'VICI', 'VOD', 'VRTS', 'VVR', 'WD', 'WIT', 'WPC', 'WSM', 'WTBA', 'WVFC', 'XRAY', 'YVR', 'ZEN', 'ZIONP']
+    for i in tester(0.05, "PEP", 5):
+        if i in solution:
+            print(i)
+    
+    
     # plot example of graph
     # Driver Code
 #    print(bfs(adjacency_list, 'AAPL', 2))   # function calling
--- group13/utils.py
+++ group13/utils.py
@@ -1,6 +1,5 @@
 from functools import lru_cache
 
-
 class Graph():
     """ A data structure for Graph """
 
@@ -21,7 +20,9 @@ def qsort(arr):
 
 import queue
 
-def bfs(graph, node): #function for BFS
+#adjacency_list = {'AMAT': ['GILD'], 'AVGO': ['ORLY', 'KHC', 'EBAY', 'CSX', 'BKNG'], 'TMUS': ['QCOM', 'COST', 'REGN', 'ADBE'], 'QCOM': ['TMUS', 'COST', 'GILD', 'REGN', 'ADBE'], 'ROST': ['SBUX', 'PYPL', 'TXN'], 'ISRG': ['SBUX', 'CMCSA', 'ADI', 'INTU', 'ILMN', 'ADP', 'NXPI'], 'ORLY': ['AVGO', 'KHC', 'WBA', 'EBAY', 'CSX', 'BKNG'], 'SBUX': ['ROST', 'ISRG', 'PYPL', 'TXN', 'INTU', 'ILMN', 'NXPI'], 'MU': ['GOOGL', 'INTC', 'FB', 'PEP', 'MDLZ', 'NFLX'], 'LRCX': ['AAPL', 'VRTX', 'MSFT', 'CHTR'], 'CMCSA': ['ISRG', 'ADI', 'INTU', 'ILMN', 'ADP', 'NXPI'], 'GOOGL': ['MU', 'INTC', 'FB', 'PEP', 'MDLZ'], 'ATVI': ['VRTX', 'CHTR'], 'COST': ['TMUS', 'QCOM', 'GILD', 'REGN', 'ADBE'], 'CSCO': ['WBA', 'CSX'], 'NVDA': [], 'AMZN': ['FISV', 'AMGN', 'XEL'], 'PYPL': ['ROST', 'SBUX', 'TXN', 'NXPI'], 'TXN': ['ROST', 'SBUX', 'PYPL', 'NXPI'], 'INTC': ['MU', 'GOOGL', 'FB', 'PEP', 'MDLZ'], 'GILD': ['AMAT', 'QCOM', 'COST', 'ADBE'], 'AAPL': ['LRCX', 'BIIB', 'MSFT'], 'CTSH': ['MAR'], 'VRTX': ['LRCX', 'ATVI', 'CHTR'], 'REGN': ['TMUS', 'QCOM', 'COST', 'ADBE'], 'ADI': ['ISRG', 'CMCSA', 'INTU', 'ILMN', 'ADP'], 'FISV': ['AMZN', 'AMGN', 'XEL'], 'BIIB': ['AAPL', 'MSFT'], 'KHC': ['AVGO', 'ORLY', 'EBAY', 'BKNG'], 'MSFT': ['LRCX', 'AAPL', 'BIIB'], 'AMD': [], 'INTU': ['ISRG', 'SBUX', 'CMCSA', 'ADI', 'ILMN', 'ADP', 'NXPI'], 'CHTR': ['LRCX', 'ATVI', 'VRTX'], 'FB': ['MU', 'GOOGL', 'INTC', 'PEP', 'MDLZ', 'NFLX'], 'ILMN': ['ISRG', 'SBUX', 'CMCSA', 'ADI', 'INTU', 'ADP', 'NXPI'], 'PEP': ['MU', 'GOOGL', 'INTC', 'FB', 'MDLZ'], 'MDLZ': ['MU', 'GOOGL', 'INTC', 'FB', 'PEP', 'NFLX'], 'NFLX': ['MU', 'FB', 'MDLZ', 'AMGN'], 'AMGN': ['AMZN', 'FISV', 'NFLX', 'XEL'], 'ADBE': ['TMUS', 'QCOM', 'COST', 'GILD', 'REGN'], 'WBA': ['ORLY', 'CSCO', 'CSX', 'BKNG'], 'EBAY': ['AVGO', 'ORLY', 'KHC', 'BKNG'], 'ADP': ['ISRG', 'CMCSA', 'ADI', 'INTU', 'ILMN', 'NXPI'], 'CSX': ['AVGO', 'ORLY', 'CSCO', 'WBA', 'BKNG'], 'MAR': ['CTSH'], 'BKNG': ['AVGO', 'ORLY', 'KHC', 'WBA', 'EBAY', 'CSX'], 'TSLA': [], 'XEL': ['AMZN', 'FISV', 'AMGN'], 'NXPI': ['ISRG', 'SBUX', 'CMCSA', 'PYPL', 'TXN', 'INTU', 'ILMN', 'ADP']}
+
+def bfs_adhoc(graph, node): #function for BFS
 #    print('BFS of adjacency list with root: {}'.format(node))
     queue = []     #Initialize a queue
     visited = []
@@ -30,7 +31,7 @@ def bfs(graph, node): #function for BFS
     visited.append(node)
     queue.append(node)
     l = 1
-    while queue:        # Creating loop to visit each node
+    while queue:      # Creating loop to visit each node
         m = queue.pop(0) 
         lvl_container[l] = []
         for neighbour in graph[m]:
@@ -39,16 +40,45 @@ def bfs(graph, node): #function for BFS
                     marked.append(m)
 #                print(str(m)+' has neighbour: '+str(neighbour)+' (level: {}) '.format(l))
                 lvl_container[l].append(neighbour)
-                lvl_container[l] = qsort(lvl_container[l])
                 visited.append(neighbour)
                 queue.append(neighbour)
-        if lvl_container[l] == '' or l==10:
-            break
+        lvl_container[l] = qsort(lvl_container[l])
         if graph[m] != [] and m in marked:
             l += 1
-    for i in range(l,11):
-        lvl_container[i] = []
+        if l > 10:
+            break
     return lvl_container
+
+def bfs(graph, node, lvl): #function for BFS
+#    print('BFS of adjacency list with root: {}'.format(node))
+    queue = []     #Initialize a queue
+    visited = []
+    lvl_container = {}
+    marked = []
+    visited.append(node)
+    queue.append(node)
+    l = 1
+    while queue:      # Creating loop to visit each node
+        m = queue.pop(0) 
+        lvl_container[l] = []
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                if m not in marked:
+                    marked.append(m)
+#                print(str(m)+' has neighbour: '+str(neighbour)+' (level: {}) '.format(l))
+                lvl_container[l].append(neighbour)
+                visited.append(neighbour)
+                queue.append(neighbour)
+        lvl_container[l] = qsort(lvl_container[l])
+        if graph[m] != [] and m in marked:
+            l += 1
+        if l > lvl:
+            break
+    return lvl_container[lvl]
+
+#if __name__ == '__main__':
+#    print(bfs(adjacency_list, 'GOOGL', 2))
+
 
 '''
 def bfs_old(s):
