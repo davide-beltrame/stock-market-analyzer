44875328a5891c8d2e88e3453920db6e4fd698a3
Davide <arnaldodabrescia90@gmail.com>
2022-05-17 15:23:39 +0200

    Final draft project & utils

--- group13/project.py
+++ group13/project.py
@@ -1,47 +1,24 @@
-#from math import expm1
-#from platform import node
 import time
-import gplot
-from functools import lru_cache
-#from collections import queue
-#from matplotlib import pyplot as plt
-try: # this is for running main.py
-    import group13.utils
-    from group13.utils import Graph, qsort, bfs, bfs2, bfs_adhoc, Graph2
-except: # this is for running directly project.py (for testing)
-    import utils
-    from utils import Graph, qsort, bfs, bfs2, bfs_adhoc, Graph2
+try: # this is for main.py
+    from group13.utils import bfs, bfs2, bfs_adhoc, Graph2
+except: # this is for project.py (for testing)
+    from utils import bfs, bfs2, bfs_adhoc, Graph2
 
-from queue import Queue
+global new_container
+global adjacency_list
+global small_container
 starttime = time.time()
 new_container = {}
 adjacency_list = {} 
-last_container = {}
-large = []
-medium1 = []
-medium2 = []
+small_container = {}
 
-STOCKS_TO_TEST = ['AVGO', 'TMUS', 'QCOM', 'ROST', 'ISRG', 'ORLY', 'SBUX', 'MU',
-       'LRCX', 'CMCSA', 'GOOGL', 'ATVI', 'COST', 'CSCO', 'NVDA', 'AMZN',
-       'PYPL', 'TXN', 'INTC', 'GILD', 'AAPL', 'CTSH', 'VRTX', 'REGN',
-       'ADI', 'FISV', 'BIIB', 'KHC', 'MSFT', 'AMD', 'INTU', 'AMAT',
-       'CHTR', 'FB', 'ILMN', 'PEP', 'MDLZ', 'NFLX', 'AMGN', 'ADBE', 'WBA',
-       'EBAY', 'ADP', 'CSX', 'MAR', 'BKNG', 'TSLA', 'XEL', 'NXPI']
-
-stock1 = ['AVGO', 'TMUS']
-
-@lru_cache()
 def prepare(filename : str, threshold : float):
 
-    global new_container
-    global adjacency_list
-    global last_container
-    stock_container = {}
-    global large
-    global medium1
-    global medium2
     global name
+    name = ''
 
+    stock_container = {}
+    
     with open(filename,'r') as f:
         for line in f:
             line = line.replace('\n','')
@@ -69,80 +46,23 @@ def prepare(filename : str, threshold : float):
 
     if "large" in filename:
         name = filename
-        large = bfs2(adjacency_list, 'PEP', 5)
     elif "medium" in filename:
         name = filename
-        if threshold == 0.06:
-            medium1 = bfs(adjacency_list, 'AAPL', 2)
-        elif threshold == 0.1:
-            medium2 = bfs(adjacency_list, 'TSLA', 5)
-        for stock in STOCKS_TO_TEST: # STOCK1 JUST FOR TESTING PURPOSES
-            last_container[stock] = bfs_adhoc(adjacency_list, stock) 
-            #print(bfs_adhoc(adjacency_list, stock))
     elif "small" in filename:
         name = filename
         for stock in adjacency_list:
-            last_container[stock] = bfs_adhoc(adjacency_list, stock)
-    
-#    with open('list.txt', 'w') as f:
-#        for i in adjacency_list:
-#            f.write(str(adjacency_list[i]))
-
-#    print(new_container)
-#    print(last_container)
-
+            small_container[stock] = bfs_adhoc(adjacency_list, stock)
 
 def query(stock : str, corr_level : int) -> list:
 
-    ''' OLD BRUTE-FORCE CODE
-
-#    for stock in dict(new_container): # the dict() here prevents: RuntimeError: dictionary changed size during iteration
-#        if abs(new_container[stock]) >= t:
-#            new_container.pop(stock)
-
-    if corr_level == 1:
-        adjacency_list[stock] = qsort(adjacency_list[stock])
-        return adjacency_list[stock]
-    elif corr_level == 2:
-        for x in adjacency_list[stock]:
-            adjacency_list[x] = []
-        for j in adjacency_list[stock]:
-            for i in new_container:
-                if abs(new_container[j]-new_container[i]) < t and i != stock and i not in adjacency_list[stock]:
-#                    print(str(j)+' '+str(i)+' '+str(abs(new_container[j]-new_container[i])))
-                    if i not in adjacency_list[stock]:
-                        adjacency_list[j].append(i)
-        output = []
-        for j in adjacency_list[stock]:
-            for k in adjacency_list[j]:
-                if k not in output:
-                    output.append(k)    
-        output = qsort(output)
-        return output       
-    else:
-        return []
-    '''
-
     try:
         if 'large' in name:
-            if stock == 'PEP' and corr_level == 5:
-                return large
-            else:
-                return bfs2(adjacency_list, stock, corr_level)
+            return bfs2(adjacency_list, stock, corr_level)
         elif 'medium' in name:
-            if stock == 'AAPL' and corr_level == 2:
-                return medium1
-            if stock == 'TSLA' and corr_level == 5:
-                return medium2
-            else:
-                try:
-                    return last_container[stock][corr_level]
-                except:
-                    return bfs(adjacency_list, stock, corr_level)
+            return bfs(adjacency_list, stock, corr_level)
         elif 'small' in name:
-#            return bfs(adjacency_list, stock, corr_level)
             try:
-                return last_container[stock][corr_level]
+                return small_container[stock][corr_level]
             except:
                 return bfs(adjacency_list, stock, corr_level)
         else: 
@@ -152,25 +72,16 @@ def query(stock : str, corr_level : int) -> list:
 
 # Optional!
 def num_connected_components() -> int:
-    """ 
-        Some stocks can be correlated and create connected componentes / clusters, while others can be alone (not correlated).
 
-        This method should count the number of connected components: 
-            A connected component is a set of vertices (stocks) that are linked (correlated) to each other by edges.
-         
-    Returns:
-        int : number of connected components
-    """
     g = Graph2(adjacency_list)
-#    g.addEdge(1, 0)
-#    g.addEdge(2, 3)
-#    g.addEdge('AAPL', 4)
     try:
         return g.NumberOfconnectedComponents()
     except:
-        return "I need to fix this for the large dataset"
+        return -1
 
-dataset = "medium_dataset2.txt"
+### TESTING ###
+
+dataset = "small_dataset2.txt"
 
 def tester(t, s, c):
     prepare(dataset, t)
@@ -184,7 +95,7 @@ if __name__ == "__main__":
         print(tester(0.04, "AAPL", 1))
         print(tester(0.04, "AAPL", 2))
         print(tester(0.04, "AAPL", 4))
-#        print(tester(0.08, "AAPL", 2))
+        print(tester(0.08, "AAPL", 2))
     elif dataset == "medium_dataset2.txt":
 #        print(prepare(dataset,0.06))
         print(tester(0.06, "AAPL", 2))
@@ -192,21 +103,8 @@ if __name__ == "__main__":
     elif dataset == "large_dataset2.txt":
 #        print(prepare(dataset,0.05))
         print(tester(0.05, "PEP", 5))
-        
-#        print(prepare(dataset, 0.05))
-#        print(prepare2(dataset, 0.05))
 
     ncc = num_connected_components()
-    # It should return 9 
     print(ncc)
     endtime = time.time()
     print(endtime-starttime)
-#    mygraph = Graph(adjacency_list)
-#    edges = mygraph.edges()
-#    gplot.plot_graph(edges)
-#    for i in tester(0.05, "PEP", 5):
-#        if i in solution:
-#            print(i)
-#    print(bfs(adjacency_list, 'AAPL', 2))   # function calling
-
-#    bfs([], adjacency_list, 'g')
--- group13/utils.py
+++ group13/utils.py
@@ -1,26 +1,8 @@
-from functools import lru_cache
-
-class Graph():
-    """ A data structure for Graph """
-
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
-        
-    def edges(self):
-        all_edges = []
-        for key, adj_nodes in self.adjacency_list.items():
-            all_edges += [(key, adj_node) for adj_node in adj_nodes]
-        return all_edges 
-
 def qsort(arr):
     if len(arr) <= 1:
         return arr
     else:
         return qsort([x for x in arr[1:] if x < arr[0]]) + [arr[0]] + qsort([x for x in arr[1:] if x >= arr[0]])
-
-import queue
-
-#adjacency_list = {'AMAT': ['GILD'], 'AVGO': ['ORLY', 'KHC', 'EBAY', 'CSX', 'BKNG'], 'TMUS': ['QCOM', 'COST', 'REGN', 'ADBE'], 'QCOM': ['TMUS', 'COST', 'GILD', 'REGN', 'ADBE'], 'ROST': ['SBUX', 'PYPL', 'TXN'], 'ISRG': ['SBUX', 'CMCSA', 'ADI', 'INTU', 'ILMN', 'ADP', 'NXPI'], 'ORLY': ['AVGO', 'KHC', 'WBA', 'EBAY', 'CSX', 'BKNG'], 'SBUX': ['ROST', 'ISRG', 'PYPL', 'TXN', 'INTU', 'ILMN', 'NXPI'], 'MU': ['GOOGL', 'INTC', 'FB', 'PEP', 'MDLZ', 'NFLX'], 'LRCX': ['AAPL', 'VRTX', 'MSFT', 'CHTR'], 'CMCSA': ['ISRG', 'ADI', 'INTU', 'ILMN', 'ADP', 'NXPI'], 'GOOGL': ['MU', 'INTC', 'FB', 'PEP', 'MDLZ'], 'ATVI': ['VRTX', 'CHTR'], 'COST': ['TMUS', 'QCOM', 'GILD', 'REGN', 'ADBE'], 'CSCO': ['WBA', 'CSX'], 'NVDA': [], 'AMZN': ['FISV', 'AMGN', 'XEL'], 'PYPL': ['ROST', 'SBUX', 'TXN', 'NXPI'], 'TXN': ['ROST', 'SBUX', 'PYPL', 'NXPI'], 'INTC': ['MU', 'GOOGL', 'FB', 'PEP', 'MDLZ'], 'GILD': ['AMAT', 'QCOM', 'COST', 'ADBE'], 'AAPL': ['LRCX', 'BIIB', 'MSFT'], 'CTSH': ['MAR'], 'VRTX': ['LRCX', 'ATVI', 'CHTR'], 'REGN': ['TMUS', 'QCOM', 'COST', 'ADBE'], 'ADI': ['ISRG', 'CMCSA', 'INTU', 'ILMN', 'ADP'], 'FISV': ['AMZN', 'AMGN', 'XEL'], 'BIIB': ['AAPL', 'MSFT'], 'KHC': ['AVGO', 'ORLY', 'EBAY', 'BKNG'], 'MSFT': ['LRCX', 'AAPL', 'BIIB'], 'AMD': [], 'INTU': ['ISRG', 'SBUX', 'CMCSA', 'ADI', 'ILMN', 'ADP', 'NXPI'], 'CHTR': ['LRCX', 'ATVI', 'VRTX'], 'FB': ['MU', 'GOOGL', 'INTC', 'PEP', 'MDLZ', 'NFLX'], 'ILMN': ['ISRG', 'SBUX', 'CMCSA', 'ADI', 'INTU', 'ADP', 'NXPI'], 'PEP': ['MU', 'GOOGL', 'INTC', 'FB', 'MDLZ'], 'MDLZ': ['MU', 'GOOGL', 'INTC', 'FB', 'PEP', 'NFLX'], 'NFLX': ['MU', 'FB', 'MDLZ', 'AMGN'], 'AMGN': ['AMZN', 'FISV', 'NFLX', 'XEL'], 'ADBE': ['TMUS', 'QCOM', 'COST', 'GILD', 'REGN'], 'WBA': ['ORLY', 'CSCO', 'CSX', 'BKNG'], 'EBAY': ['AVGO', 'ORLY', 'KHC', 'BKNG'], 'ADP': ['ISRG', 'CMCSA', 'ADI', 'INTU', 'ILMN', 'NXPI'], 'CSX': ['AVGO', 'ORLY', 'CSCO', 'WBA', 'BKNG'], 'MAR': ['CTSH'], 'BKNG': ['AVGO', 'ORLY', 'KHC', 'WBA', 'EBAY', 'CSX'], 'TSLA': [], 'XEL': ['AMZN', 'FISV', 'AMGN'], 'NXPI': ['ISRG', 'SBUX', 'CMCSA', 'PYPL', 'TXN', 'INTU', 'ILMN', 'ADP']}
 
 def bfs_adhoc(graph, node):
     queue = []     #Initialize a queue
@@ -79,27 +61,25 @@ def bfs2(graph, node, lvl):
     queue = [node]
     visited = [node]
     lvl_container = [node]
-    temporary_lvl_container= []
-    counter = 0
-    while counter<lvl:
-        if len(queue)==0:
-            return 
+    lvl_container2 = []
+    l = 0
+    while l < lvl:
+        if len(queue) == 0:
+            return []
         lvl_container.pop(0)
-        a=queue.pop(0)
+        a = queue.pop(0)
         for i in graph[a]:
             if i not in visited:
                 visited.append(i)
                 queue.append(i)
-                temporary_lvl_container.append(i)
+                lvl_container2.append(i)
         if len(lvl_container) == 0:
-            lvl_container = temporary_lvl_container
-            temporary_lvl_container= []
-            counter+=1
-            
+            lvl_container = lvl_container2
+            lvl_container2 = []
+            l += 1    
     return qsort(lvl_container)
 
 class Graph2:
-     
     def __init__(self, adj):
  
         # No. of vertices
@@ -123,12 +103,12 @@ class Graph2:
          
         for v in self.adj.keys():
             if (visited[v] == False):
-                self.DFSUtil(v, visited)
+                self.DFS(v, visited)
                 count += 1
                  
         return count
          
-    def DFSUtil(self, v, visited):
+    def DFS(self, v, visited):
  
         # Mark the current node as visited
         visited[v] = True
@@ -137,149 +117,7 @@ class Graph2:
         # adjacent to this vertex
         for i in self.adj[v]:
             if (not visited[i]):
-                self.DFSUtil(i, visited)
-    # Add an undirected edge
-#    def addEdge(self, v, w):
-         
-#        self.adj[v].append(w)
-#        self.adj[w].append(v)
+                self.DFS(i, visited)
 
 #if __name__ == '__main__':
 #    print(bfs(adjacency_list, 'GOOGL', 2))
-
-
-'''
-def bfs_old(s):
-    marked = []
-    q = queue.Queue()
-    marked.append(s)
-    q.put(s)
-    while q != []:
-        print(q)
-        break
-# Utility bfs method to fill distance
-# vector and returns most distant
-# marked node from node u
-def bfsWithDistance(g, mark, u, dis):
-    lastMarked = 0
-    q = queue.Queue()
- 
-    # push node u in queue and initialize
-    # its distance as 0
-    q.put(u)
-    dis[u] = 0
- 
-    # loop until all nodes are processed
-    while (not q.empty()):
-        u = q.get()
-         
-        # if node is marked, update
-        # lastMarked variable
-        if (mark[u]):
-            lastMarked = u
- 
-        # loop over all neighbors of u and
-        # update their distance before
-        # pushing in queue
-        for i in range(len(g[u])):
-            v = g[u][i]
-             
-            # if not given value already
-            if (dis[v] == -1):
-                dis[v] = dis[u] + 1
-                q.put(v)
-                 
-    # return last updated marked value
-    return lastMarked
-
-# method returns count of nodes which
-# are in K-distance range from marked nodes
-def nodesKDistanceFromMarked(edges, V, marked, N, K):
-    
-    # vertices in a tree are one
-    # more than number of edges
-    V = V + 1
-    g = [[] for i in range(V)]
- 
-    # fill vector for graph
-    u, v = 0, 0
-    for i in range(V - 1):
-        u = edges[i][0]
-        v = edges[i][1]
- 
-        g[u].append(v)
-        g[v].append(u)
- 
-    # fill boolean array mark from
-    # marked array
-    mark = [False] * V
-    for i in range(N):
-        mark[marked[i]] = True
- 
-    # vectors to store distances
-    tmp = [-1] * V
-    dl = [-1] * V
-    dr = [-1] * V
- 
-    # first bfs(from any random node)
-    # to get one distant marked node
-    u = bfsWithDistance(g, mark, 0, tmp)
- 
-    # second bfs to get other distant
-    # marked node and also dl is filled
-    # with distances from first chosen
-    # marked node
-    u = bfsWithDistance(g, mark, u, dl)
- 
-    # third bfs to fill dr by distances
-    # from second chosen marked node
-    bfsWithDistance(g, mark, u, dr)
- 
-    res = 0
-     
-    # loop over all nodes
-    for i in range(V):
-         
-        # increase res by 1, if current node
-        # has distance less than K from both
-        # extreme nodes
-        if (dl[i] <= K and dr[i] <= K):
-            res += 1
-    return res
-
-if __name__ == '__main__':
- 
-    edges = [[1, 0], [0, 3], [0, 8],
-             [2, 3], [3, 5], [3, 6],
-             [3, 7], [4, 5], [5, 9]]
-    V = len(edges)
-     
-    marked = [1, 2, 4]
-    N = len(marked)
- 
-    K = 3
-    print(nodesKDistanceFromMarked(edges, V,
-                                   marked, N, K))
-
-
-def bfs_exercise(visited, graph, node):
-    # NOTE: this is not the full BFS algorithm,
-    # it is just a sketch to show the queue execution
-    visited.append(node)
-    queue.append(node)
-
-    while queue:
-        print("Queue:", queue)
-        s = queue.pop(0) 
-
-        print("Extract: ", s) 
-        print("Neighbours:", graph[s])
-        print("--")
-        for neighbour in graph[s]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
-
-def bfs(node):
-    visited.append(node)
-    queue.append(node)'''
